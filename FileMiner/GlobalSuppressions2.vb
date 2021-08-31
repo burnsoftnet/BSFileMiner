e903633d4dd403381489217297814bc53fc3272f
Joe M <bs.github@burnsoft.net>
2021-08-31 17:05:36 -0400

    updated supression file

---
+++ FileMiner/GlobalSuppressions2.vb
@@ -0,0 +1,23 @@
+﻿' This file is used by Code Analysis to maintain SuppressMessage
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given
+' a specific target and scoped to a namespace, type, member, etc.
+
+Imports System.Diagnostics.CodeAnalysis
+
+<Assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.Database.ConnExec(System.String,System.String@)")>
+<Assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.Database.GetData(System.String,System.String@)~System.Data.DataTable")>
+<Assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.btnAdd_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.btnAdd_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.btnAdd_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.btnDelete_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.btnDelete_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.btnEdit_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.ContentsExist(System.String,System.String,System.Int32@)~System.Boolean")>
+<Assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.ContentsExist(System.String,System.String,System.Int32@)~System.Boolean")>
+<Assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.ContentsExistRegex(System.String,System.String)~System.Boolean")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.frmMain_Load(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.GenerateSaveFileName~System.String")>
+<Assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.GetExtOfFile(System.String)~System.String")>
+<Assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.SearchListToolStripMenuItem_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification:="<Pending>", Scope:="member", Target:="~M:FileMiner.FrmMain.StringToSearchToolStripMenuItem_Click(System.Object,System.EventArgs)")>
